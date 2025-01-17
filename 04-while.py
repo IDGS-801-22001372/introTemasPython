47a0ba180341e976856426c1bb90db0057b8ac96
IDGS-801-22001372 <81512@alumnos.utleon.edu.mx>
2025-01-16 23:18:02 -0600

    commit

--- 04-while.py
+++ 04-while.py
@@ -4,19 +4,26 @@ while x<10:
     print(x)
     x=x+1
 
-    ***
-        operacion de multiplicacion de a x b utilizando sumas
-        a=3
-        b=4
-        salida: 3+3+3+3=12
-    ***
+        # operacion de multiplicacion de a x b utilizando sumas
+        # a=3
+        # b=4
+        # salida: 3+3+3+3=12
 
-a=3
-b=4
-x=0
+a = 3
+b = 4
 
-while x<=12:
-    print(x)
-    x=x+a
+resultado = 0
+contador = 0
+sumas = ""
 
+while contador < b:
+    resultado += a
+    sumas += str(a)
+    contador += 1
+    if contador < b:
+        sumas += "+"
+
+sumas += "=" + str(resultado)
+
+print(sumas)
 
